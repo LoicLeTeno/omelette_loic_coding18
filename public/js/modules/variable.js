16def212c89b24e28f6aa75a3eb579179681896e
le Téno <leteno.loic@gmail.com>
2021-05-06 09:38:47 +0200

    fin object personne LLT

---
+++ public/js/modules/variable.js
@@ -0,0 +1,17 @@
+// OBJECT : personne
+    // export
+export {paul};
+
+let paul = {
+    nom : "",
+    lieu : "",
+
+    argent : 0,
+
+    mainDroite : [],
+    du_coup_mainGauche : [],
+
+    seDeplacer() {}, //(lieu)
+    payerArticle() {}, // (article)
+    couper() {} // (ingredient, outil)
+}
